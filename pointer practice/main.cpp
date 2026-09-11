2eeb8268c5800836f4be70a08976bfb80954a8b8
uf44932-bit <uf44932@mail.harpercollege.edu>
2026-09-11 11:10:38 -0500

    Initial Commit

---
+++ pointer practice/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  pointer practice
+//
+//  Created by Anahi Munoz on 9/11/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
