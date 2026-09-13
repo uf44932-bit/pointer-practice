854591b2b549c4095659af94fad9cd874f3fbc42
uf44932-bit <uf44932@mail.harpercollege.edu>
2026-09-13 10:39:21 -0500

    did the maximum arrays and starting the lengt of c-style string

--- pointer practice/main.cpp
+++ pointer practice/main.cpp
@@ -29,10 +29,34 @@ int main() {
     cout << *ptr_b << endl;
     
     
+    // Maximum Arrays
+    /*
+     Write a program to find the maximum number in an unsorted list of integers.
+
+     Create an integer array (int[]) that contains some number of random, unsorted values. You might make the values up yourself or use rand(). Then, create a pointer called max_ptr that finds and points to the maximum value in the list.
+
+     Hint: since this is an unsorted list of integers, you will need to make use of linear search to find the maximum rather than binary search.
+     */
+    
+    int arr[5] = { 1, 3, 2, 5, 4};
+    
+    int* max_ptr = arr;
+    
+    for (int* ptr= arr; ptr < arr + 5; ptr++) {
+        if (*ptr > *max_ptr) {
+            max_ptr = ptr;
+        }
+    }
+    
+    cout << "The Maximum is: " << *max_ptr << endl;
     
     
-    
-    
+    //length of a c-style string
+    /*
+     Write a program to calculate the length of a string using a pointer.
+
+     Create a C-style string which is an array of chars. Then, create a pointer that points to the start of the array. Move the pointer through the string until you reach the null character \0, and then print out how many cells it visited.
+     */
     
     return EXIT_SUCCESS;
 }
