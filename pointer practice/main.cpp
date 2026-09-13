602ec15890c4d70c258fab063a581b943c36d531
uf44932-bit <uf44932@mail.harpercollege.edu>
2026-09-13 10:13:24 -0500

    started to do the first program with the Two Integers

--- pointer practice/main.cpp
+++ pointer practice/main.cpp
@@ -1,14 +1,38 @@
-//
-//  main.cpp
-//  pointer practice
-//
-//  Created by Anahi Munoz on 9/11/26.
-//
+
 
 #include <iostream>
+#include <vector>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+using namespace std;
+
+int main() {
+  
+    //Two Integers
+    /*
+     Write a program that asks the user to enter two integers to be stored in the variables a and b. Assign the addresses of a and b to ptr_a and ptr_b. Then, print out the dereferenced values of ptr_a and ptr_b to the console.
+     */
+    
+    int a;
+    int b;
+    
+    cout << "Enter a integer for a: ";
+    cin >> a;
+    
+    cout << "Enter a integer for b: ";
+    cin >> b;
+    
+    int* ptr_a = &a;
+    int* ptr_b = &b;
+    
+    cout << *ptr_a << endl;
+    cout << *ptr_b << endl;
+    
+    
+    
+    
+    
+    
+    
     return EXIT_SUCCESS;
 }
